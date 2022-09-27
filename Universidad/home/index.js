3e0ee927e4046e2623f0cd30607fd8f86a93c6ac
oscardhf <85182165+oscardhf@users.noreply.github.com>
2022-09-27 15:25:07 -0500

    Start

---
+++ Universidad/home/index.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("login");
+input.addEventListener("click",function(e){
+    e.preventDefault()
+    console.log("algo 1")
+    window.location.href="/home/Inicio.html"
+})
+console.log("algo 2")
